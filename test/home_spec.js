207dfc28782cfed927df9dcc3caceab35f03d7a6
oliverpople <oliverpople@gmail.com>
2017-11-30 15:41:23 +0000

    Passed feature test for play move button

--- test/home_spec.js
+++ test/home_spec.js
@@ -4,7 +4,7 @@ var browser = new Browser();
 
 describe("intialising home page", function() {
 
-// first test whether our browser is created and site is actually accessible
+  // first test whether our browser is created and site is actually accessible
   it("should have defined headless browser", function(next) {
     expect(typeof browser != "undefined").toBe(true);
     expect(browser instanceof Browser).toBe(true);
@@ -14,15 +14,24 @@ describe("intialising home page", function() {
   it("should visit the homepage and see 9 slots", function(next) {
     browser.visit(url, function(err) {
       expect(browser.success).toBe(true);
-  expect(browser.query("input[name='slot1']")).not.toBeNull();
-        expect(browser.query("input[name='slot2']")).not.toBeNull();
-          expect(browser.query("input[name='slot3']")).not.toBeNull();
-          expect(browser.query("input[name='slot4']")).not.toBeNull();
-          expect(browser.query("input[name='slot5']")).not.toBeNull();
-          expect(browser.query("input[name='slot6']")).not.toBeNull();
-          expect(browser.query("input[name='slot7']")).not.toBeNull();
-          expect(browser.query("input[name='slot8']")).not.toBeNull();
-          expect(browser.query("input[name='slot9']")).not.toBeNull();
+      expect(browser.query("input[name='slot1']")).not.toBeNull();
+      expect(browser.query("input[name='slot2']")).not.toBeNull();
+      expect(browser.query("input[name='slot3']")).not.toBeNull();
+      expect(browser.query("input[name='slot4']")).not.toBeNull();
+      expect(browser.query("input[name='slot5']")).not.toBeNull();
+      expect(browser.query("input[name='slot6']")).not.toBeNull();
+      expect(browser.query("input[name='slot7']")).not.toBeNull();
+      expect(browser.query("input[name='slot8']")).not.toBeNull();
+      expect(browser.query("input[name='slot9']")).not.toBeNull();
+      next();
+    });
+  });
+
+  it("should have a submit button", function(next) {
+    browser.visit(url, function(err) {
+      expect(browser.success).toBe(true);
+      expect(browser.query("input[type='submit']")).not.toBeNull();
+      expect(browser.query("input[value='Play Move']")).not.toBeNull();
       next();
     });
   });
